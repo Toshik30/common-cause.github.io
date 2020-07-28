1106d518f91836e71a3cba4c34874d616765aff1
Toshik30 <56067717+Toshik30@users.noreply.github.com>
2020-07-28 14:01:04 +0300

    php

---
+++ thanks.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Получите свою прибыль уже сегодня</title>
+    <link rel="shortcut icon" href="img/favicon.ico" type="image/x-icon">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,600,700&amp;subset=cyrillic,cyrillic-ext" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/thanks.min.css">
+	<!-- Yandex.Metrika counter obsachfebeadelo.ru -->
+            <script type="text/javascript" >
+                (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
+                    m[i].l=1*new Date();k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+                (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    
+                ym(64980856, "init", {
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true
+                });
+            </script>
+            <noscript><div><img src="https://mc.yandex.ru/watch/64980856" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+        <!-- /Yandex.Metrika counter --> </head>
+<body>
+<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=2579314379050465&ev=Lead&noscript=1"/>    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <p>
+                    В течении 24 часов на указанный email придет письмо с подробной инструкцией.<br />
+                    Если письмо не появилось в папке «Входящие», проверьте папку «Спам».<br />
+                    Если вы не получили письмо, пожалуйста, обратитесь в тех. поддержку. После получения письма:<br />
+                    1. Добавьте адрес отправителя в контакты<br />
+                    2. Если письмо попало в папку «Спам», выделите его и нажмите «Не спам», чтобы в дальнейшем гарантированно получать всю важную информацию по проекту «Общее дело».<br />
+                    Не забудьте нажать кнопку "Ок"
+                </p>
+            </div>
+            <div class="col-md-12">
+                <div class="button">
+                    <a href="/">На главную</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
